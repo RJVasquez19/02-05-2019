15ac26012c3b46ca1a06e753e32c50cca520353b
RJVasquez19 <47983895+RJVasquez19@users.noreply.github.com>
2019-05-14 15:17:50 -0600

    Add files via upload

---
+++ BaseDatos/capaNegocios/manttoVendor.cs
@@ -0,0 +1,30 @@
+﻿using capaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaNegocios
+{
+    public class manttoVendor
+    {
+        bdConex test = new bdConex();
+
+        public String prueba() 
+        {
+            String mensaje;
+
+            if(test.Conectar() == true)
+            {
+                test.Desconectar();
+                mensaje = "Conexion realizada";
+            }else
+            {
+                test.Desconectar();
+                mensaje = "Conexion no realizada";
+            }
+            return mensaje;
+        }
+    }
+}
---
+++ BaseDatos/capaPresentacion/frmVendor.cs
@@ -0,0 +1,34 @@
+﻿using capaNegocios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capaPresentacion
+{
+    public partial class frmVendor : Form
+    {
+
+        manttoVendor obj = new manttoVendor();
+
+        public frmVendor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVendor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(obj.prueba());
+        }
+    }
+}
